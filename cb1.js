09e9058b69a82acea7623d6e0abe272200f0a51c
chendi1020 <chendi1020@gmail.com>
2016-04-19 10:17:51 -0400

    dropdown works

--- cb1.js
+++ cb1.js
@@ -1,27 +1,15 @@
 var data = ["Undergraduate", "Graduate", "Professional"];
 
-var select = d3.select('body')
+var select = d3.select('#filter')
   .append('select')
   	.attr('class','select')
     .on('change',onchange)
+    
 
 var options = select
   .selectAll('option')
-	.data(data).enter()
+	.data(data)
+    .enter()
 	.append('option')
 		.text(function (d) { return d; });
 
-function onchange() {
-	var selected = this.value;
-    //selectValue = d3.select('select').property('value')
-    displayOthers = this.checked ? "inline" : "none";
-      display = this.checked ? "none" : "inline";
-    
-	//d3.select('body')
-	//	.append('p')
-	//	.text(selectValue + ' is the last selected option.')
-    console.log(selected);
-   
-};
-
-  //console.log(onchange().selectValue);
--- table.js
+++ table.js
@@ -1,25 +1,66 @@
+
+                 
 d3.text("enrollmentfile.csv", function(data) {
                 var parsedCSV = d3.csv.parseRows(data);
-                 var parsedCSV1= [];
-                 for (var i=0;i<parsedCSV.length;i++){
-                    if((parsedCSV[i][2]==="Undergraduate" && parsedCSV[i][0]==='FS15')|| parsedCSV[i]==parsedCSV[0] ){
-                        parsedCSV1.push(parsedCSV[i]);
-                    }
-                    
-                }
-                 
-               
-
-                var container = d3.select("#dvData")
+       graph =  d3.select("#dvData")
                     .append("table")
 
-                    .selectAll("tr")
-                        .data(parsedCSV1)
+                     .selectAll("tr")
+                        .data(parsedCSV)
                         .enter()
                         .append("tr")
+
                         
-                    .selectAll("td")
+                     .selectAll("td")
                         .data(function(d) { return d; }).enter()
                         .append("td")
                         .text(function(d) { return d; });
-            });
+    
+});
+
+
+
+function onchange() {
+	//var selected = this.value;
+    selectValue = d3.select('select')[0][0].value
+   
+  
+     var parsedCSV1= [];
+    d3.text("enrollmentfile.csv", function(data) {
+                var parsedCSV = d3.csv.parseRows(data);
+                  console.log(selectValue);
+                 for (var i=0;i<parsedCSV.length;i++){
+                    if((parsedCSV[i][2]===selectValue && parsedCSV[i][0]==='FS15')|| parsedCSV[i]==parsedCSV[0] ){
+                        parsedCSV1.push(parsedCSV[i]);
+                        }   
+                    };
+        
+      
+                function update(){
+                  //  console.log(parsedCSV1);
+                var container = d3.select("#dvData");
+                var rows =  container.selectAll("tr")
+                        .data(parsedCSV1);
+                        
+                    
+                     rows.enter().append("tr");
+                        
+                var cells = rows.selectAll("td")
+                        .data(function(d) { return d; });
+                    
+                    cells.enter().append("td");
+                    
+                    cells.text(function(d) { return d; });
+                    //cells.exit().remove();
+                    rows.exit().remove();
+                    
+               
+                };
+        
+                 
+        
+              update();
+            }); 
+   
+};
+
